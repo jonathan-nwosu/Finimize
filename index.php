b65c3331e58730d5e58ae3360f62947db9135365
Jonathan <jonathan.nwosu@outlook.com>
2017-12-04 20:54:31 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+
+<html>
+
+	<head>
+		
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				
+			</header>
+
+		
+
+		<!-- Footer -->
+			<footer id="footer">
+				
+			</footer>
+
+		<!-- Scripts -->
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+
+</html>
+
+
